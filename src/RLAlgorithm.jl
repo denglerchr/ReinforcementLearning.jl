fbaecab8349575871037033ab87eddce16d72ea7
Dengler <c.dengler@tum.de>
2019-05-07 10:50:31 +0200

    Limit std to be a number

--- src/RLAlgorithm.jl
+++ src/RLAlgorithm.jl
@@ -23,6 +23,7 @@ function logpdfgaussian(meanU, U, std::Number)
     # Cannot use sum(x->x^2, ..., dims = 1) yet, as AutoGrad doesn support it
     Temp = sum( abs2.(U.-meanU) , dims=1)
     logpdf = c .- Temp ./std^2 ./2
+
     return logpdf
 end
 
@@ -33,7 +34,7 @@ function pdfgaussianquot(meanU1, meanU2, U, std1, std2)
 end
 
 # Return p1(U)/p2(U) for Gaussian distributions, but cut off to max_val (avoid Inf)
-function pdfgaussianquot_limited(meanU1, meanU2, U, std1, std2, max_val::Number)
+function pdfgaussianquot_limited(meanU1, meanU2, U, std1::Number, std2::Number, max_val::Number)
     logpdf = min.( log(max_val), logpdfgaussian(meanU1, U, std1) .- logpdfgaussian(meanU2, U, std2) )
     return exp.(logpdf)
 end
