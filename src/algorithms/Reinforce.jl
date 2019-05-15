20a08f2c774b0061602012301fb08f6ba8767563
Dengler <c.dengler@tum.de>
2019-05-15 14:20:39 +0200

    Big changes in the structure for Reinforce. Also many intermediatze results are normalized easier step size adaptation. Changes only change gradient norm, not direction.

--- src/algorithms/Reinforce.jl
+++ src/algorithms/Reinforce.jl
@@ -14,10 +14,11 @@ Reinforce(Ntraj::Int, Nsteps::Number, Nepisodes::Int, gamma::Number, baseline::S
 
 
 # This is used to generate data batches using the iterate function
-struct ReinforceIterator
+struct ReinforceIterator{P<:Policy}
     env::Environment
-    pol::Policy
+    pol::P
     rl::Reinforce
+    costvec::Vector
 end
 
 # Main function
@@ -28,8 +29,8 @@ function minimize!(rl::Reinforce, pol::Policy, env::Environment)
     costvec = [NaN for i = 1:rl.Nepisodes]
 
     # Define loss and batch generator
-    loss(X, U, r, episN, printbool) = logpdfr!(pol, rl.baseline, X, U, r, episN, printbool, costvec)
-    rlit = ReinforceIterator(env, pol, rl)
+    loss(X, U, R) = logpdfr!(pol, X, U, R)
+    rlit = ReinforceIterator(env, pol, rl, costvec)
 
     # Compute gradient using backprop and apply gradient
     Knet.minimize!(loss, rlit, pol.optimizer)
@@ -46,36 +47,83 @@ function iterate(rlit::ReinforceIterator)
     trajvec = gettraj(rlit.pol, rlit.env, rlit.rl)
 
     # Transform the data into big tensors for faster GPU computation
-    X, U, r = stacktraj(trajvec, rlit.pol)
-    applydiscount!(r, rlit.rl.gamma)
+    X, U, R = reinforceinputdata(trajvec, rlit.pol, rlit.rl.gamma, rlit.rl.baseline, true, 1, rlit.costvec)
+
     atype = rlit.pol.atype
-    return (atype(X), atype(U), atype(r), 1, true), 1
+    return (atype(X), atype(U), atype(R)), 1
 end
 
 function iterate(rlit::ReinforceIterator, episodeN::Int)
     # Finish after sampling Nepisodes episodes
     episodeN >= rlit.rl.Nepisodes && return nothing
 
+    # If not finished, get new trajectories etc.
     newepisN = episodeN+1
-
-    # Decide if the loss function call should print something or not
     printbool = ( mod(newepisN, rlit.rl.printevery) == 0)
 
-    # If not finished, get new trajectories etc. (Same as above)
-    print("\rEpis. $newepisN\r")
+    print("\rEpis. $newepisN")
     trajvec = gettraj(rlit.pol, rlit.env, rlit.rl)
-    X, U, r = stacktraj(trajvec, rlit.pol)
-    applydiscount!(r, rlit.rl.gamma)
+    X, U, R = reinforceinputdata(trajvec, rlit.pol, rlit.rl.gamma, rlit.rl.baseline, printbool, newepisN, rlit.costvec)
+
     atype = rlit.pol.atype
-    return (atype(X), atype(U), atype(r), newepisN, printbool), newepisN
+    return (atype(X), atype(U), atype(R)), newepisN
 end
 
 length(it::ReinforceIterator) = it.rl.Nepisodes
 
 
+function reinforceinputdata(trajvec::Vector{Trajectory}, pol::StaticPolicy, gamma::Number, baseline::Symbol, printbool::Bool, episN::Int, costvec::Vector)
+
+    X, U, r = stackXUr(trajvec, pol)
+    applydiscount!(r, gamma)
+
+    # Total reward
+    R = sum(r, dims = 3)
+    meanR = mean(R)
+    printbool && println("\rEpis. $episN, mean costs: $meanR")
+    costvec[episN] = meanR
+    if baseline == :mean
+        R .-= meanR
+    elseif baseline == :normalize
+        R .-= meanR
+        R ./= Statistics.std(R)
+    elseif baseline != :none
+        @error("Unknown baseline $baseline")
+    end
+
+    return X, U, R
+end
+
+function reinforceinputdata(trajvec::Vector{Trajectory}, pol::RecurrentPolicy, gamma::Number, baseline::Symbol, printbool::Bool, episN::Int, costvec::Vector)
+    X, U = stackXU(trajvec, pol, pol.seqlength)
+    r = stackr(trajvec, pol)
+    applydiscount!(r, gamma)
+
+    # Total reward
+    R = sum(r, dims = 3)
+    meanR = mean(R)
+    printbool && println("\rEpis. $episN, mean costs: $meanR")
+    costvec[episN] = meanR
+    if baseline == :mean
+        R .-= meanR
+    elseif baseline == :normalize
+        R .-= meanR
+        R ./= Statistics.std(R)
+    elseif baseline != :none
+        @error("Unknown baseline $baseline")
+    end
+
+    # Need to replicate R to the corresponding slices
+    N = floor(Int, size(r, 3)/pol.seqlength )
+    R = repeat(R, inner = (1, N, 1))
+
+    return X, U, R
+end
+
+
 ## The actual gradient generating functions
 # log(p)*R without baseline.
-function logpdfr!(pol::Policy, baseline::Symbol, X::T, U::T, r::T, episN::Int, printbool::Bool, costvec::AbstractVector) where {T<:Union{AbstractArray, KnetArray}}
+function logpdfr!(pol::Policy, X::T, U::T, R::T) where {T<:Union{AbstractArray, KnetArray}}
 
     # mean of the policy when observing these states
     resetpol!(pol)
@@ -83,20 +131,7 @@ function logpdfr!(pol::Policy, baseline::Symbol, X::T, U::T, r::T, episN::Int, p
 
     # log-probability of taking the U's that were actually taken
     p = logpdfgaussian(mU, U, pol.std)
-    p = sum(p, dims = 3)
-
-    # Total reward
-    R = sum(r, dims = 3)
-    meanR = mean(R)
-    costvec[episN] = meanR
-    if baseline == :mean
-        R .-= meanR
-    elseif baseline != :none
-        @error("Unknown baseline $baseline")
-    end
-
-    # Print if requested
-    printbool && println("Epis. $episN: mean costs: $meanR")
+    p = mean(p, dims = 3)
 
     # Return the mean of the product, no baseline applied
     return mean(R .* p)
